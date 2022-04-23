05e77ef2189a8e414dbf33396f6e86ab9c06d41a
Zoe McCarthy <zoemccarthy12@gmail.com>
2022-04-23 05:57:38 -0400

    more diagnostic, generating a low res but accurate sim grid from a high resolution sim function

--- test/pybamm_generate_all_models_sims.jl
+++ test/pybamm_generate_all_models_sims.jl
@@ -9,6 +9,7 @@ begin
     using FiniteDifferences
     using ModelingToolkit, Symbolics, DomainSets
     import ModelingToolkit: Interval, infimum, supremum
+    using IterTools
     using Infiltrator
 end
 
@@ -38,11 +39,21 @@ solvars = Dict([(var=>sim.solution.__getitem__(var)) for var in variables]...)
 solcsp = solvars["X-averaged positive particle concentration"]
 solcsn = solvars["X-averaged negative particle concentration"]
 csn(t, r) = solcsn(t=t*0.15930183773127454 * solcsn.timescale, r=r*solcsn.length_scales["negative particle size"], x=0.0)[1]
+#csn(t, r) = solcsn(t=t * solcsn.timescale, r=r*solcsn.length_scales["negative particle size"], x=0.0)[1]
 csntf(t) = begin csnr(r) = csn(t, r) end
 csp(t, r) = solcsp(t=t*0.15930183773127454 * solcsp.timescale, r=r*solcsp.length_scales["positive particle size"], x=1.0)[1]
 csptf(t) = begin cspr(r) = csp(t, r) end
 @show abs(central_fdm(5, 1, max_range=1e-6)(csntf(1), 1) - (-0.14182855923368468))
 @show abs(central_fdm(5, 1, max_range=1e-6)(csntf(1), 0) - 0)
+@show abs(forward_fdm(5, 1)(csntf(1), 0) - 0)
+@show abs(backward_fdm(5, 1)(csntf(1), 1) - (-0.14182855923368468))
 @show abs(central_fdm(5, 1, max_range=1e-6)(csptf(1), 1) - (0.03237700710041634))
 @show abs(central_fdm(5, 1, max_range=1e-6)(csptf(1), 0) - 0)
+@show abs(forward_fdm(5, 1)(csptf(1), 0) - 0)
+@show abs(backward_fdm(5, 1)(csptf(1), 1) - (0.03237700710041634))
 @show central_fdm(5, 1, max_range=1e-6)(csptf(1), 1)
+ts = range(Interval(0,1), 100)
+csn_accurate = map(tr->csn(tr[1], tr[2]), collect(product(ts, ts)))
+csp_accurate = map(tr->csp(tr[1], tr[2]), collect(product(ts, ts)))
+@show (csn_accurate[end, end] - csn_accurate[end, end-1]) / (ts[end] - ts[end-1])
+@show (csp_accurate[end, end] - csp_accurate[end, end-1]) / (ts[end] - ts[end-1])
