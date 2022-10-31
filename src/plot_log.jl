3a506fdbc7b06815ea54688068f3957de62d859e
Zoe McCarthy <zoemccarthy12@gmail.com>
2022-10-31 02:30:38 -0700

    added l1 error logging

--- src/plot_log.jl
+++ src/plot_log.jl
@@ -1,3 +1,6 @@
+norm2(x) = LinearAlgebra.norm(x, 2)
+norm1(x) = LinearAlgebra.norm(x, 1)
+
 function get_eval_network_at_sim_data_func(model, chains)
     sim_data = read_sim_data(model)
     ivs = keys(sim_data[:ivs])
@@ -6,6 +9,8 @@ function get_eval_network_at_sim_data_func(model, chains)
     num_dvs = length(dvs)
     ground_truths = map(dv -> sim_data[:dvs][dv], dvs)
     ground_truths_norm = LinearAlgebra.norm.(ground_truths)
+    ground_truths_l2 = norm2.(ground_truths)
+    ground_truths_l1 = norm1.(ground_truths)
     mdf = MultiDimensionalFunction(chains, VectorOfParameterizedMDFApplyFuncType(), ivs, dvs, dv_deps)
 
     function eval_network_at_sim_data(p)
@@ -27,12 +32,13 @@ function get_eval_network_at_sim_data_func(model, chains)
         #@show size.(ground_truths)
 
         errors = map(i -> network_evals[i] .- ground_truths[i], 1:num_dvs)
-        errors_norm = LinearAlgebra.norm.(errors)
-        errors_rel = map(i -> errors_norm[i] / ground_truths_norm[i], 1:num_dvs)
+        errors_l2 = norm2.(errors)
+        errors_l1 = norm1.(errors)
+        errors_rel_l2 = map(i -> errors_l2[i] / ground_truths_l2[i], 1:num_dvs)
+        errors_rel_l1 = map(i -> errors_l1[i] / ground_truths_l1[i], 1:num_dvs)
         #@infiltrate
 
-        return (dvs=dvs, network_evals=network_evals, errors=errors,
-            errors_norm=errors_norm, errors_rel=errors_rel)
+        return (; dvs, network_evals, errors, errors_l2, errors_l1, errors_rel_l2, errors_rel_l1)
     end
 end
 
@@ -43,15 +49,19 @@ function get_cb_func(model, log_frequency)
         cb = function (p, l)
             if iteration[1] % log_frequency == 0
                 res = eval_network_at_sim_data(p)
-                error_rel_max = max(res[:errors_rel]...)
+                error_rel_l2_max = max(res[:errors_rel_l2]...)
+                error_rel_l1_max = max(res[:errors_rel_l1]...)
                 #@show res[:errors_rel]
                 #@show typeof(res[:errors_rel])
                 #@show error_rel_max
                 #@show typeof(error_rel_max)
-                NeuralPDE.logscalar(logger, error_rel_max, "errors/max_error_relative", iteration[1])
+                NeuralPDE.logscalar(logger, error_rel_l2_max, "errors/max_error_l2_relative", iteration[1])
+                NeuralPDE.logscalar(logger, error_rel_l1_max, "errors/max_error_l1_relative", iteration[1])
                 for (i, dv) in enumerate(res[:dvs])
-                    NeuralPDE.logscalar(logger, res[:errors_norm][i], "errors/$(string(dv))_error_norm", iteration[1])
-                    NeuralPDE.logscalar(logger, res[:errors_rel][i], "errors/$(string(dv))_error_relative", iteration[1])
+                    NeuralPDE.logscalar(logger, res[:errors_l2][i], "errors/$(string(dv))_error_l2", iteration[1])
+                    NeuralPDE.logscalar(logger, res[:errors_l1][i], "errors/$(string(dv))_error_l1", iteration[1])
+                    NeuralPDE.logscalar(logger, res[:errors_rel_l2][i], "errors/$(string(dv))_error_relative_l2", iteration[1])
+                    NeuralPDE.logscalar(logger, res[:errors_rel_l1][i], "errors/$(string(dv))_error_relative_l1", iteration[1])
                 end
                 println("Iteration $(iteration[1]), loss $(l)")
             end
